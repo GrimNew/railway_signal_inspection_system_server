d3559937734083d7fa362725e0505e01ca81826f
GrimNew <326408489@qq.com>
2018-03-29 17:40:48 +0800

    Support SSLSocket

--- src/InspectServer.java
+++ src/InspectServer.java
@@ -1,6 +1,8 @@
+import javax.net.ssl.*;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.security.*;
+import java.security.cert.CertificateException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -10,13 +12,16 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+import static javax.net.ssl.SSLContext.*;
+
 public class InspectServer {
-    private static final String url=
+    private static final String url =
             "jdbc:mysql://localhost:3306/inspect" +//JDBC方式/MySQL数据库/本机/端口3306/数据库名称
                     "?useSSL=false&useUnicode=true&characterEncoding=utf8";//SSL关闭/使用Unicode编码/编码方式utf-8
-    private static final String username="root";
-    private static final String password="root";
-    public static void main(String[] args) {
+    private static final String username = "root";
+    private static final String password = "root";
+
+    public static void main(String[] args) throws IOException {
         //创建线程池等待任务队列，数量10
         BlockingQueue<Runnable> blockingQueue = new ArrayBlockingQueue<>(10);
         //创建线程池，基本线程5个，最大20个，存活时间2分钟
@@ -30,22 +35,49 @@ public class InspectServer {
             e.printStackTrace();
         }
 
+        SSLSocket sslSocket = null;
+        SSLServerSocket sslServerSocket = null;
         try (//连接数据库对象实例化
              Connection connection = DriverManager.getConnection(url, username, password);
              //获取statement数据库操作实例
-             Statement statement = connection.createStatement();
-             //serverSocket实例绑定60001端口
-             ServerSocket serverSocket = new ServerSocket(60001)) {
+             Statement statement = connection.createStatement()
+        ) {
+            //实例化SSL上下文对象，使用TLS协议(SSLv1-v3已经不安全被弃用，现阶段使用TLSv1.0-v1.2，API 19及以下不支持TLSv1.1和v1.2)
+            SSLContext sslContext = getInstance("TLS");
+            //设置秘钥库管理器，Sun公司遵守X509标准的SunX509
+            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
+//            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance("SunX509");
+            //装载服务器秘钥库，PKCS12对密钥对和证书管理，安全级别较高
+            KeyStore keyStore = KeyStore.getInstance("PKCS12");
+            keyStore.load(new FileInputStream("src/server.p12"), "server".toCharArray());
+//            //装载客户端信任的秘钥库
+//            KeyStore trustKeyStore = KeyStore.getInstance("PKCS12");
+//            trustKeyStore.load(new FileInputStream("src/client_trust.p12"), "client".toCharArray());
+            //初始化秘钥管理器，密码为server
+            keyManagerFactory.init(keyStore, "server".toCharArray());
+//            trustManagerFactory.init(trustKeyStore);
+            //初始化SSL上下文对象，信任库管理器为空（双向验证时使用），指定安全的随机数作为加密数
+            sslContext.init(keyManagerFactory.getKeyManagers(), null, new SecureRandom());
 
+            //serverSocket实例绑定60001端口
+            sslServerSocket = (SSLServerSocket) sslContext.getServerSocketFactory().createServerSocket(60001);
+//            //启用双向验证
+//            sslServerSocket.setNeedClientAuth(true);
             while (statement != null) {
                 //启动侦听并进入阻塞状态
-                Socket socket = serverSocket.accept();
+                sslSocket = (SSLSocket) sslServerSocket.accept();
                 //启动线程
-                threadPoolExecutor.execute(new InspectServerThread(statement, socket));
+                threadPoolExecutor.execute(new InspectServerThread(statement, sslSocket));
             }
-
-        } catch (SQLException | IOException e) {
+        } catch (SQLException | NoSuchAlgorithmException | KeyStoreException | CertificateException | IOException | UnrecoverableKeyException | KeyManagementException e) {
             e.printStackTrace();
+        } finally {
+            if (sslServerSocket != null) {
+                sslServerSocket.close();
+            }
+            if (sslSocket != null) {
+                sslSocket.close();
+            }
         }
     }
 }
--- src/InspectServerThread.java
+++ src/InspectServerThread.java
@@ -1,50 +1,51 @@
+import javax.net.ssl.SSLSocket;
 import java.io.*;
-import java.net.Socket;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class InspectServerThread implements Runnable{
+public class InspectServerThread implements Runnable {
     //创建对象
     private Statement statement;
-    private Socket socket;
+    private SSLSocket sslSocket;
 
     //构造函数完成初始化类
-    InspectServerThread(Statement statement, Socket socket){
+    InspectServerThread(Statement statement, SSLSocket sslSocket) {
         this.statement = statement;
-        this.socket=socket;
+        this.sslSocket = sslSocket;
     }
+
     @Override
     public void run() {
         String[] splitMessage;  //存储拆分消息字符串数组
         String message; //消息字符串
         try (//创建输入流对象
-             InputStream inputStream = socket.getInputStream();
+             InputStream inputStream = sslSocket.getInputStream();
              //读取输入流
              InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
              //缓存输入流
              BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-             OutputStream outputStream = socket.getOutputStream();
+             OutputStream outputStream = sslSocket.getOutputStream();
              PrintWriter printWriter = new PrintWriter(outputStream)) {
             //注：一次读取一行，发送时必须要换行
             message = bufferedReader.readLine();
             //调用split()方法以“#”拆分若干段
             splitMessage = message.split("#");
-            switch (splitMessage[0]){
+            switch (splitMessage[0]) {
                 //请求码"0"，即登陆操作
-                case "0":{
+                case "0": {
                     //对象实例化，完成SQL语句执行
                     ResultSet resultSet = statement.executeQuery(
                             "SELECT * FROM user_table WHERE username=\'" + splitMessage[1] + "\' AND password=\'" + splitMessage[2] + "\'");
                     //查询数据库返回结果，有结果则用户合法返回"1"或"2"，否则用户非法返回其他
                     if (resultSet.next()) {
                         //admin权限位为true时，返回管理员代码"0"，否则返回用户代码"1"
-                        if(resultSet.getBoolean(3)){
+                        if (resultSet.getBoolean(3)) {
                             printWriter.println("0");
-                        }else {
+                        } else {
                             printWriter.println("1");
                         }
-                    }else {
+                    } else {
                         printWriter.println("2");
                     }
                     printWriter.flush();
@@ -54,13 +55,11 @@ public class InspectServerThread implements Runnable{
                 case "1":
 
                     break;
-                //请求码"2"，即信息支持操作
+                //请求码"2"，即其他操作
                 case "2":
 
                     break;
             }
-
-
         } catch (IOException | SQLException e) {
             e.printStackTrace();
         }
