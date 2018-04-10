7bb9c088b65f7a5b43706b591ae9f8d72f747d14
GrimNew <326408489@qq.com>
2018-04-10 10:14:55 +0800

    fixed request code logic

--- src/InspectServerThread.java
+++ src/InspectServerThread.java
@@ -17,8 +17,10 @@ public class InspectServerThread implements Runnable {
 
     @Override
     public void run() {
-        String[] splitMessage;  //存储拆分消息字符串数组
+        String[] splitMessage; //存储拆分消息字符串数组
         String message; //消息字符串
+        String T_SQL;
+        String username = "";
         try (//创建输入流对象
              InputStream inputStream = sslSocket.getInputStream();
              //读取输入流
@@ -27,38 +29,51 @@ public class InspectServerThread implements Runnable {
              BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
              OutputStream outputStream = sslSocket.getOutputStream();
              PrintWriter printWriter = new PrintWriter(outputStream)) {
+
             //注：一次读取一行，发送时必须要换行
-            message = bufferedReader.readLine();
-            //调用split()方法以“#”拆分若干段
-            splitMessage = message.split("#");
-            switch (splitMessage[0]) {
-                //请求码"0"，即登陆操作
-                case "0": {
-                    //对象实例化，完成SQL语句执行
-                    ResultSet resultSet = statement.executeQuery(
-                            "SELECT * FROM user_table WHERE username=\'" + splitMessage[1] + "\' AND password=\'" + splitMessage[2] + "\'");
-                    //查询数据库返回结果，有结果则用户合法返回"1"或"2"，否则用户非法返回其他
-                    if (resultSet.next()) {
-                        //admin权限位为true时，返回管理员代码"0"，否则返回用户代码"1"
-                        if (resultSet.getBoolean(3)) {
-                            printWriter.println("0");
+            while ((message = bufferedReader.readLine()) != null) {
+                //调用split()方法以“#”拆分若干段
+                splitMessage = message.split("#");
+                switch (splitMessage[0]) {
+                    //请求码"0"，登陆操作
+                    case "0": {
+                        //对象实例化，完成SQL语句执行
+                        ResultSet resultSet = statement.executeQuery(
+                                "SELECT * FROM user_table WHERE username=\'" + splitMessage[1] + "\' AND password=\'" + splitMessage[2] + "\'");
+                        //查询数据库返回结果，有结果则用户合法返回"00"或"01"，否则用户非法返回"02"
+                        if (resultSet.next()) {
+                            //admin权限位为true时，返回管理员代码"00"，否则返回用户代码"01"
+                            if (resultSet.getBoolean(3)) {
+                                printWriter.println("00");
+                            } else {
+                                printWriter.println("01");
+                            }
+                            username = splitMessage[1];
                         } else {
-                            printWriter.println("1");
+                            printWriter.println("02");
                         }
-                    } else {
-                        printWriter.println("2");
+                        printWriter.flush();
+                        break;
                     }
-                    printWriter.flush();
-                    break;
+                    //请求码"1"，更新设备数据
+                    case "1": {
+                        T_SQL="INSERT INTO data_table(device_id,username,status) VALUES(\'"
+                                +splitMessage[1]+"\',\'"+username+"\',\'"+splitMessage[2]+"\')";
+                        //执行T-SQL语句，成功执行返回代码"10",执行失败则返回代码"11"
+                        if(statement.executeUpdate(T_SQL) != 0) {
+                            printWriter.println("10");
+                        }else {
+                            printWriter.println("11");
+                        }
+                        printWriter.flush();
+                        break;
+                    }
+                    //请求码"2"，
+                    case "2": {
+
+                        break;
+                    }
                 }
-                //请求码"1"，即信息支持操作
-                case "1":
-
-                    break;
-                //请求码"2"，即其他操作
-                case "2":
-
-                    break;
             }
         } catch (IOException | SQLException e) {
             e.printStackTrace();
